74fa2cc0b808e85cf0552738d064f5eb66ca8663
palla023 <palla02323@gmail.com>
2025-03-13 21:44:22 +0530

    urls replaced with render

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,11 +1,16 @@
 import axios from 'axios';
+const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000/api"
+    : "https://mern-social-server-92ic.onrender.com/api"; 
+
 
 export const loginCall = async (userCredentials, dispatch) => {
     dispatch({
         type: 'LOGIN_START',
     })
     try {
-        const res = await axios.post("/auth/login", userCredentials);
+        const res = await axios.post(`${BASE_URL}/auth/login`, userCredentials);
         dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
         // console.log(res.data)
         // alert(res.message)
